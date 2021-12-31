3eeb9294595682217f8debf65cbbd9152fff5224
Rizky Sedyanto <rizky.sedyanto@wgs-id.com>
2021-12-31 11:33:53 +0700

    feat(cache): changed to use url

--- configs/redis.config.js
+++ configs/redis.config.js
@@ -1,7 +1,3 @@
 const Redis = require('ioredis')
 
-exports.redisClient = new Redis({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASSWORD,
-})
+exports.redisClient = new Redis(process.env.REDIS_URL)

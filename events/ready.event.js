6ace7b038f70a082916e7bb5c73a3516ab63a91f
Rizky Sedyanto <rizky.sedyanto@wgs-id.com>
2021-12-31 11:34:11 +0700

    feat(api): added connection test

--- events/ready.event.js
+++ events/ready.event.js
@@ -1,10 +1,14 @@
 const { discordClient } = require("../configs/discord.config");
+const { getApi } = require('../utils/getApi.util');
 
 module.exports = {
   name: 'ready',
   once: true,
-  execute(client) {
+  async execute(client) {
     discordClient.user.setPresence({ activities: [{ name: 'Black Desert Online' }], status: 'online' });
+    // checking api connection
+    const api = await getApi(`${process.env.API_URL}/`)
+    if (api) {console.log(`Api ready!`)}
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },

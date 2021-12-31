a4e204af5f734b8f1bb4b7e562afceef00329ead
Rizky Sedyanto <rizky.sedyanto@wgs-id.com>
2021-12-31 10:22:55 +0700

    chore: removed unecessary data

--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const sendBossNotification = require('./sendBossNotification.js');
-const sendTimeCycleChange = require('./sendTimeCycleChange.js');
-
-module.exports = {
-  sendBossNotification,
-  sendTimeCycleChange,
-}
